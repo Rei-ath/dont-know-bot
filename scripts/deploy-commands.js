f29ac8c0b42701818b0e4cbbaff7285a2ef93fde
Rei <hk350456@gmail.com>
2023-07-01 00:18:24 +0530

    fix

--- scripts/deploy-commands.js
+++ scripts/deploy-commands.js
@@ -1,6 +1,6 @@
 const { REST, Routes } = require('discord.js');
 const clientId = require('../config.json').clientId || process.env['clientId'];
-const token = require('./config.json').token || process.env['token'];
+const token = require('../config.json').token || process.env['token'];
 const fs = require('node:fs');
 const path = require('node:path');
 const commands = [];
