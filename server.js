632abb67c0f931e44785e1915002e776e82be722
Rei <hk350456@gmail.com>
2023-07-08 04:58:36 +0530

    fix

--- server.js
+++ server.js
@@ -1,14 +1,14 @@
 const express = require('express');
 const server = express();
 
-server.all('/',(req,res)=>{
-  res.send('<h>Server Vroom Vroom</h>')
+server.all('/', (req, res) => {
+	res.send('<h>Server Vroom Vroom</h>');
 });
 
 
-module.exports=()=>{
-  server.listen(6969,()=>{
-    console.log('yeeee')
-  });
-  return true;
-}
+module.exports = () => {
+	server.listen(6969, () => {
+		console.log('yeeee');
+	});
+	return true;
+};
